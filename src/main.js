f0c9f1158a506a5407437b401f3fbe475c0ad6c1
shonburgundy <seanbeloussow@yahoo.com>
2020-06-24 13:44:27 -0700

    doing gods work

--- src/main.js
+++ src/main.js
@@ -1,1 +1,55 @@
-yeah t
+import { changeState, stateChanger } from './character.js';
+
+// const startHealth = changeState("health")(100)
+// 
+
+const p1starthealth = changeState("p1health")(100);
+const p2starthealth = changeState("p2health")(100);
+
+const p2heal = changeState("p2health")(Math.floor(Math.random() * Math.floor(10))); 
+
+const p1heal = changeState("p1health")(Math.floor(Math.random() * Math.floor(10)));
+
+// Button generates random number but only generates one random number till reload, need to figure out how to fix that
+
+const p1attack = changeState("p2health")(Math.floor(Math.random() * Math.floor(-10)));
+
+const p2attack = changeState("p1health")(Math.floor(Math.random() * Math.floor(-10)));
+
+
+$(document).ready(function() {
+  
+  const p1StartHealthState = stateChanger(p1starthealth);
+  $('#p1-health-value').text(p1StartHealthState.p1health);
+
+  const p2StartHealthState = stateChanger(p2starthealth);
+  $('#p2-health-value').text(p2StartHealthState.p2health);
+
+  // ------ PLAYER 1 ATTACK & HEAL  ------
+  $('#p1heal').click(function(){
+    const newState = stateChanger(p1heal);
+    $('#p1-health-value').text(newState.p1health);
+  });
+
+  $('#p1-attack').click(function(){
+    const newState = stateChanger(p1attack);
+    $('#p2-health-value').text(newState.p2health);
+  });
+
+  // ------ PLAYER 2 ATTACK & HEAL  ------
+  $('#p2heal').click(function(){
+    const newState = stateChanger(p2heal);
+    $('#p2-health-value').text(newState.p2health);
+  });
+
+  $('#p2-attack').click(function(){
+    const newState = stateChanger(p2attack);
+    $('#p1-health-value').text(newState.p1health);
+  });
+
+  // Button generates random number but only generates one random number till reload, need to figure out how to fix that
+  // Player turns?
+  // Game win parameter
+  // Other shit we gotta do
+
+});
